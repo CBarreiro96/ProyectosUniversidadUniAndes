271dea7528e71b3ae58c2b21e5cb03c12eab878a
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2022-10-20 22:56:52 -0500

    Modificaciones dinámicas al HTML

--- proyectoTypescript/main.ts
+++ proyectoTypescript/main.ts
@@ -2,4 +2,19 @@ import { Aprendiz, NivelEducativo } from "./aprendiz.js";
 
 export const ap = new Aprendiz("Juan pablo","Reyes Gómez","avatar.png",30,NivelEducativo.POSGRADO)
 
-console.log(ap);
+/* console.log(ap); */
+/*El ! me indica que el valor puede ser null*/
+
+let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
+
+mostrarDatosAprendiz(ap);
+
+function mostrarDatosAprendiz(aprendiz: Aprendiz):void{
+    let tbodyAprendiz = document.createElement("tbody")
+    tbodyAprendiz.innerHTML = `<tr><td colspan=2><img src="./${aprendiz.avatar}" height="100"></td></tr>
+    <tr><td>Nombres:</td><td>${aprendiz.nombres}</td></tr>
+    <tr><td>Apellidos:</td><td>${aprendiz.apellidos}</td></tr>
+    <tr><td>Nivel Educativo:</td><td>${aprendiz.nivelEducativo}</td></tr>
+    <tr><td>Nombres:</td><td>${aprendiz.edad}</td></tr>`
+    aprendizTable.appendChild(tbodyAprendiz);
+}
